53dc3e4e5db8b4ecea72c55c30cf4ba64644ae54
dmkur <72811384+dmkur@users.noreply.github.com>
2023-09-27 12:58:43 +0000

    Update 9 files, updated react-router-dom to v6

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,49 +1,48 @@
-import {useState} from "react";
-import {useDispatch} from "react-redux";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
 
-import {authActions} from "../../redux";
-import {useHistory} from "react-router-dom";
+import { authActions } from "../../redux";
+import {useNavigate} from "react-router-dom";
 
 const Login = () => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const dispatch = useDispatch();
-    const history = useHistory();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const handleClick = (e) => {
-        e.preventDefault();
-        const {error} = dispatch(authActions.login({username, password}));
+  const handleClick = (e) => {
+    e.preventDefault();
+    const { error } = dispatch(authActions.login({ username, password }));    
+    if (!error) navigate('/')
+  };
 
-        if (!error) history.push('/')
-    };
-
-    return (
-        <div
-            style={{
-                height: "100vh",
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                justifyContent: "center",
-            }}
-        >
-            <input
-                style={{padding: 10, marginBottom: 20}}
-                type="text"
-                placeholder="username"
-                onChange={(e) => setUsername(e.target.value)}
-            />
-            <input
-                style={{padding: 10, marginBottom: 20}}
-                type="password"
-                placeholder="password"
-                onChange={(e) => setPassword(e.target.value)}
-            />
-            <button onClick={handleClick} style={{padding: 10, width: 100}}>
-                Login
-            </button>
-        </div>
-    )
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <input
+        style={{ padding: 10, marginBottom: 20 }}
+        type="text"
+        placeholder="username"
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        style={{ padding: 10, marginBottom: 20 }}
+        type="password"
+        placeholder="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleClick} style={{ padding: 10, width: 100 }}>
+        Login
+      </button>
+    </div>
+  );
 };
 
-export {Login}
+export { Login };
